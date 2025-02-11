2136afece871790d1d97d44b256157a990bb844a
roxxe3 <hamzafarissi@gmail.com>
2025-02-11 13:30:56 +0100

    Integrate Google Generative AI for movie content embedding and update Supabase configuration

--- config.js
+++ config.js
@@ -1,16 +1,41 @@
-import OpenAI from 'openai';
+import { GoogleGenerativeAI } from "@google/generative-ai";
 import { createClient } from "@supabase/supabase-js";
+import {GEMENI_API_KEY, SUPERBASE_API_KEY} from './env.js';
+import movies from "./content.js";
 
-/** OpenAI config */
-if (!process.env.OPENAI_API_KEY) throw new Error("OpenAI API key is missing or invalid.");
-export const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-  dangerouslyAllowBrowser: true
-});
-
-/** Supabase config */
-const privateKey = process.env.SUPABASE_API_KEY;
+const privateKey = SUPERBASE_API_KEY;
 if (!privateKey) throw new Error(`Expected env var SUPABASE_API_KEY`);
-const url = process.env.SUPABASE_URL;
+const url = "https://kdpejypwdvwgophpylqm.supabase.co";
 if (!url) throw new Error(`Expected env var SUPABASE_URL`);
 export const supabase = createClient(url, privateKey);
+
+
+const genAI = new GoogleGenerativeAI(GEMENI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "text-embedding-004" });
+
+async function run(input) {
+    try {
+        const data = await Promise.all(input.map(async movie => {
+            const result = await model.embedContent(movie.content);
+            const obj = {
+                content: movie.content,
+                embedding: result.embedding.values
+            };
+            return obj;
+        }));
+        await supabase.from('documents').insert(data);
+    } catch (error) {
+        console.error("Error during embedding:", error);
+    }
+}
+
+export async function queryMovies(input) {
+    const result = await model.embedContent(input);
+    const embedding = result.embedding.values;
+    const { data } = await supabase.rpc('match_documents', {
+        query_embedding: embedding,
+        match_threshold: 0.40,
+        match_count: 1
+      }); 
+      console.log(data)
+}
